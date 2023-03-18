14e173e0152148a5927ffb1432df6044976cef86
rbit <44907670+rbitzigeio@users.noreply.github.com>
2023-03-18 17:28:52 +0100

    Delete ERD_SP_InsertBandwidth.sql

--- ERDStatistics/database/scripts/ERD_SP_InsertBandwidth.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertBandwidth`(IN uTime int, IN fTime timestamp, IN bitsIn double, IN bitsOut double, IN reportID int, IN itSystem int)
-BEGIN 
-   INSERT INTO ERD.Bandwidth (UnixTime, FormattedTime, BitsPerSecIn, BitsPerSecOut, ReportID, ITSystem)
-   VALUES (uTime, fTime, bitsIn, bitsOut, reportID, itSystem);
-END
