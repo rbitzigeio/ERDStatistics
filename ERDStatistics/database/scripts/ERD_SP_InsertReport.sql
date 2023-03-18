9b2d496ad813f70652cae2a8aa319624732b25d5
rbit <44907670+rbitzigeio@users.noreply.github.com>
2023-03-18 17:28:59 +0100

    Delete ERD_SP_InsertReport.sql

--- ERDStatistics/database/scripts/ERD_SP_InsertReport.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertReport`(IN pTitle varchar(255), IN pDescription varchar(255), IN pID int, IN pDate date, IN pSection varchar(80), IN pLineChart varchar(255), IN pFileName varchar(255), IN pITSystem int )
-BEGIN 
-   INSERT INTO ERD.Report (Title, Description, ID, CreationDate, Section, LineChart, FileName, ITSystem)
-   VALUES (pTitle, pDescription, pID, pDate, pSection, pLineChart, pFileName, pITSystem);
-END
