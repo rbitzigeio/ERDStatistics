e34fad5279f4679fd446e807ec0e9e318eff7c23
rbit <44907670+rbitzigeio@users.noreply.github.com>
2023-03-21 16:02:03 +0000

    Update SP
    Check insert of reports and bandwidth

--- ERDStatistics/database/scripts/ERD_Commands.sql
+++ ERDStatistics/database/scripts/ERD_Commands.sql
@@ -2,12 +2,17 @@ use erd;
 select * from itsystem;
 select * from report;
 select * from bandwitdth;
+select * from itsystem;
 select count(*) from itsystem;
-select count(*) from report;
-select count(*) from bandwidth;
-delete from bandwidth;
-delete from report;
-delete from itsystem;
+select count(*) from report where ITSystem=9;
+select count(*) from bandwidth where itsystem=9;
+#-----------------------------------------------
+# DELETE
+delete from bandwidth where itsystem=9;
+delete from report where ITSystem=9;
+delete from itsystem where ID=9;
+#-----------------------------------------------
+# DROP
 drop procedure ERD.insertBandwidth;
 drop procedure ERD.insertReport;
 drop function ERD.checkReport;
--- ERDStatistics/database/scripts/ERD_SP.sql
+++ ERDStatistics/database/scripts/ERD_SP.sql
@@ -18,3 +18,55 @@ BEGIN
 END //
 
 Delimiter ;
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `printAllITSystems`()
+BEGIN
+    declare _sizeR int;
+    declare _sizeB int;
+    declare _id int;
+    declare _name VARCHAR(32);
+    declare cursor_ID int;
+    declare cursor_Name VARCHAR(32);
+    declare done INT default FALSE;
+    declare cursor_i CURSOR FOR select id, name FROM ITSYSTEM;
+    declare continue handler for not found set done = TRUE;
+	drop table if exists ERD.tmpStatistik; 
+    create table if not exists ERD.tmpStatistik ( 
+	   ID INT,
+       NAME Varchar(32),
+       REPORT INT,
+       BANDWIDTH INT  
+    );
+    
+    OPEN cursor_i;
+    read_loop: LOOP
+        FETCH cursor_i INTO cursor_ID, cursor_Name;
+        if done THEN
+            leave read_loop;
+		end if;
+       
+        select count(*) into _sizeR from report where ITSystem = cursor_ID;
+		select count(*) into _sizeB from bandwidth where ITSystem = cursor_ID;
+    
+	    INSERT INTO ERD.tmpStatistik (ID, NAME, REPORT, BANDWIDTH)
+	    VALUES (cursor_ID, cursor_Name, _sizeR, _sizeB); 
+    END LOOP;
+    CLOSE cursor_i;
+    
+    select * from erd.tmpStatistik;
+END //
+
+Delimiter ;
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `printITSystem`(IN pName VARCHAR(32))
+BEGIN
+	declare _sizeR int;
+    declare _sizeB int;
+    declare _id int;
+	select id into _id from itsystem where name = pName;
+    select count(*) into _sizeR from report where ITSystem = _id;
+    select count(*) into _sizeB from bandwidth where ITSystem = _id;
+    select concat('Reports : ', _sizeR, ' Bandwidth : ', _sizeB, ' ID : ',  _id);
+END//
+
+Delimiter ;
