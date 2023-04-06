e06d82eef210f1ad214904a2a111ad3f57715518
rbit <44907670+rbitzigeio@users.noreply.github.com>
2023-04-06 16:44:27 +0200

    New sql commands

--- ERDStatistics/database/scripts/ERD_Commands.sql
+++ ERDStatistics/database/scripts/ERD_Commands.sql
@@ -6,20 +6,27 @@ select * from bandwidtth;
 select count(*) from bandwidth where ITSystem=(select ID from ITSystem where name ="OPTRA");
 select count(*) from bandwidth group by ITSystem order by ITSystem;
 select count(*) from report group by ITSystem order by ITSystem;
-select * from bandwidth where ITSystem=(select ID from ITSystem where name ="AKOM") order by UnixTime desc;
+select * from bandwidth where ITSystem=(select ID from ITSystem where name ="AKOM") and Date(FormattedTime) = "2023-04-04";
+select * from bandwidth where ITSystem=(select ID from ITSystem where name ="AKOM") and UnixTime=1680560100 order by UnixTime desc;
 select * from bandwidth where ITSystem=1 and UnixTime=1679786040;
 select * from itsystem;
 select count(*) from itsystem;
 select count(*) from report where ITSystem=7;
-select count(*) from bandwidth;
-select * from bandwidth where ITSystem = (select ID from ITSystem where name ="AKOM") and UnixTime=1679954700;
+select * from bandwidth where Date(FormattedTime) ="2023-04-04" and ITSystem=2;
+select count(*) from bandwidth where ITSystem=2 and ReportID=281700;
+select count(*) from bandwidth where Date(FormattedTime) ="2023-04-03" and ITSystem=1;
+select * from bandwidth where FormattedTime="2023-04-04 00:16:00" and ITSystem=2;
+select * from report where CreationDate ="2023-04-04" order by ID;
+select * from bandwidth where ITSystem = (select ID from ITSystem where name ="AKOM") and UnixTime=1680560100;
 select * from bandwidth where ITSystem = (select ID from ITSystem where name ="AKOM") order by FormattedTime;
 select distinct(Date(FormattedTime)) from bandwidth where ITSystem = (select ID from ITSystem where name ='AKOM') order by Date(FormattedTime);
 select * from bandwidth where ITSystem = (select ID from ITSystem where name ='AKOM') and Date(FormattedTime) between '2023-03-01' and '2023-03-02';
 #-----------------------------------------------
 # DELETE
 delete from bandwidth where itsystem=7;
+delete from bandwidth where Date(FormattedTime) ="2023-04-05";
 delete from report where ITSystem=7;
+delete from report where CreationDate ="2023-04-04";
 delete from itsystem where ID=15;
 delete from bandwidth where ITSystem=2 and UnixTime=1679954700 and ReportID=279978;
 #-----------------------------------------------
